c581b0d695a1e3322882bccbb0e7e6d7ec33c691
Abrahão Lopes <117474324+abrahao2k@users.noreply.github.com>
2023-07-11 11:12:30 -0300

    Login Completo
    Abrir nova janela ao clicar em um botão

--- login.py
+++ login.py
@@ -7,33 +7,42 @@
 
 
 from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QMessageBox
+
+#Conexão com o BD
+import mysql.connector
+conexao = mysql.connector.connect(
+    host="localhost", user="root",
+    password="", database="escola")
+cursor = conexao.cursor()
+print("Conectado ao BD.")
 
 
 class Ui_Login(object):
     def setupUi(self, Login):
         Login.setObjectName("Login")
-        Login.resize(262, 93)
+        Login.resize(244, 93)
         self.centralwidget = QtWidgets.QWidget(parent=Login)
         self.centralwidget.setObjectName("centralwidget")
         self.formLayout = QtWidgets.QFormLayout(self.centralwidget)
         self.formLayout.setObjectName("formLayout")
-        self.label = QtWidgets.QLabel(parent=self.centralwidget)
-        self.label.setObjectName("label")
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label)
+        self.label_usuario = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label_usuario.setObjectName("label_usuario")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label_usuario)
         self.line_usuario = QtWidgets.QLineEdit(parent=self.centralwidget)
         self.line_usuario.setObjectName("line_usuario")
         self.formLayout.setWidget(0, QtWidgets.QFormLayout.ItemRole.FieldRole, self.line_usuario)
-        self.label_2 = QtWidgets.QLabel(parent=self.centralwidget)
-        self.label_2.setObjectName("label_2")
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label_2)
+        self.label_senha = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label_senha.setObjectName("label_senha")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label_senha)
         self.line_senha = QtWidgets.QLineEdit(parent=self.centralwidget)
         self.line_senha.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
         self.line_senha.setObjectName("line_senha")
         self.formLayout.setWidget(1, QtWidgets.QFormLayout.ItemRole.FieldRole, self.line_senha)
-        self.botao_login = QtWidgets.QPushButton(parent=self.centralwidget)
-        self.botao_login.setObjectName("botao_login")
-        self.botao_login.clicked.connect(self.login)
-        self.formLayout.setWidget(2, QtWidgets.QFormLayout.ItemRole.FieldRole, self.botao_login)
+        self.botao_entrar = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.botao_entrar.setObjectName("botao_entrar")
+        self.botao_entrar.clicked.connect(self.login)
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.ItemRole.FieldRole, self.botao_entrar)
         Login.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(Login)
@@ -41,30 +50,49 @@ class Ui_Login(object):
 
     def retranslateUi(self, Login):
         _translate = QtCore.QCoreApplication.translate
-        Login.setWindowTitle(_translate("Login", "Entrada do Sistema"))
-        self.label.setText(_translate("Login", "Usuario:"))
-        self.label_2.setText(_translate("Login", "Senha:"))
-        self.botao_login.setText(_translate("Login", "LOGIN"))
-    
+        Login.setWindowTitle(_translate("Login", "Login do Sistema"))
+        self.label_usuario.setText(_translate("Login", "Usuário:"))
+        self.label_senha.setText(_translate("Login", "Senha:"))
+        self.botao_entrar.setText(_translate("Login", "ENTRAR"))
+
     def login(self):
-        usuario = self.line_usuario.text()
-        senha   = self.line_senha.text()
+        nome = self.line_usuario.text()
+        senha = self.line_senha.text()
         
-        if usuario == "admin" and senha == "nimda":
-            from cadaluno import Ui_CadAluno
-            self.tela = QtWidgets.QMainWindow()
-            self.cad  = Ui_CadAluno()
-            self.cad.setupUi(self.tela)
-            self.tela.show()
-            Login.close()
+        sql = '''SELECT * FROM usuario WHERE nome = %s
+        AND senha = MD5(%s)'''
+        cursor.execute(sql, (nome, senha))
+        dados = cursor.fetchall()
         
-        else:
-            from PyQt6.QtWidgets import QMessageBox
+        if len(dados) == 0 : # Não encontrou
             msg = QMessageBox()
             msg.setWindowTitle("Aviso")
             msg.setText("Acesso negado.")
             msg.exec()
-
+        
+        else:
+            #importar a classe da tela que deseja abrir
+            from cadaluno import Ui_CadAluno
+            # criar uma tela vazia
+            self.tela = QtWidgets.QMainWindow()
+            # criar um objeto da tela que deseja exibir
+            self.cadastro = Ui_CadAluno()
+            # associar a tela vazia com o código da tela a exibir
+            self.cadastro.setupUi(self.tela)
+            # exibir a nova tela
+            self.tela.show()
+            # fechar a tela de login
+            Login.close()
+                        
+            
+            
+            
+            '''
+            msg = QMessageBox()
+            msg.setWindowTitle("Aviso")
+            msg.setText("Acesso liberado.")
+            msg.exec()
+            '''
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
